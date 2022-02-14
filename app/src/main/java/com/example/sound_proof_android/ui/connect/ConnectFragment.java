8bd059f3dac1e88b12ed2b980cfeb90a32a864c1
Wooseok Lee <111leews@gmail.com>
2022-02-13 22:04:04 -0500

    Implemented a quick UI for connect fragment

---
+++ app/src/main/java/com/example/sound_proof_android/ui/connect/ConnectFragment.java
@@ -0,0 +1,38 @@
+package com.example.sound_proof_android.ui.connect;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sound_proof_android.R;
+
+public class ConnectFragment extends Fragment {
+
+    private ConnectViewModel mViewModel;
+
+    public static ConnectFragment newInstance() {
+        return new ConnectFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.connect_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ConnectViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/sound_proof_android/ui/connect/ConnectViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sound_proof_android.ui.connect;
+
+import androidx.lifecycle.ViewModel;
+
+public class ConnectViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
