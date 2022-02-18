b6abbc352d715ab426cfd8cb11333c77d3470170
Wooseok Lee <111leews@gmail.com>
2022-02-18 00:54:41 -0500

    Create/Encrypt/Decrypt Implemented for Connect Fragment

--- app/src/main/java/com/example/sound_proof_android/ui/connect/ConnectFragment.java
+++ app/src/main/java/com/example/sound_proof_android/ui/connect/ConnectFragment.java
@@ -1,22 +1,57 @@
 package com.example.sound_proof_android.ui.connect;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.util.Base64;
+import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.sound_proof_android.R;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.spec.RSAKeyGenParameterSpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class ConnectFragment extends Fragment {
 
     private ConnectViewModel mViewModel;
+    private EditText browserText;
+    private Button submitButton;
+    private TextView pubKeyText;
+    // RSA variables
+    private static final String TAG = "RSACryptor";
+    private static final String CIPHER_ALGORITHM = "RSA/ECB/PKCS1Padding";
+    private String test = "abcdefghijkhmlopqrstuvwxyz";
+    byte[] encrypted;
+    String decrypted;
+    KeyPairGenerator keyPairGenerator;
+    KeyPair kp;
 
     public static ConnectFragment newInstance() {
         return new ConnectFragment();
@@ -25,14 +60,106 @@ public class ConnectFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.connect_fragment, container, false);
+        View v = inflater.inflate(R.layout.fragment_connect, container, false);
+
+        browserText = v.findViewById(R.id.browserText);
+        submitButton = v.findViewById(R.id.submitButton);
+        pubKeyText = v.findViewById(R.id.pubKeyText);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Create Key");
+                createKey();
+                KeyStore keyStore = null;
+                try {
+                    keyStore = KeyStore.getInstance("AndroidKeyStore");
+                } catch (KeyStoreException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    keyStore.load(null);
+                } catch (CertificateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+                PublicKey publicKey = null;
+                try {
+                    publicKey = keyStore.getCertificate("key1").getPublicKey();
+                } catch (KeyStoreException e) {
+                    e.printStackTrace();
+                }
+                Log.d(TAG,"publicKey1"+publicKey);
+                String pubkey3 = publicKey.getEncoded().toString();
+                Log.d(TAG,"publicKey3"+ pubkey3);
+                Log.d(TAG,"publicKey2"+Base64.encodeToString(publicKey.getEncoded(), Base64.DEFAULT));
+            }
+        });
+        return v;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ConnectViewModel.class);
-        // TODO: Use the ViewModel
+    public void createKey(){
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance(
+                    KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+            keyPairGenerator.initialize(
+                    new KeyGenParameterSpec.Builder(
+                            "key1",
+                            KeyProperties.PURPOSE_DECRYPT | KeyProperties.PURPOSE_ENCRYPT)
+                            .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                            .setAlgorithmParameterSpec(new RSAKeyGenParameterSpec(2048, RSAKeyGenParameterSpec.F4))
+                            .setDigests(KeyProperties.DIGEST_SHA256 , KeyProperties.DIGEST_SHA512)
+                            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+                            .build());
+            kp = keyPairGenerator.generateKeyPair();
+        }
+        catch (Exception e){
+            Log.e(TAG, ""+e);
+        }
+
+    }
+
+    public byte[] encrypt(final String text) throws NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException, KeyStoreException, CertificateException, IOException {
+        Log.d(TAG, "encrypt test" + text+"");
+        KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+        keyStore.load(null);
+        PublicKey publicKey = keyStore.getCertificate("key1").getPublicKey();
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+
+        Log.d(TAG,"publicKey1"+publicKey);
+        String pubkey3 = publicKey.getEncoded().toString();
+        Log.d(TAG,"publicKey3"+ pubkey3);
+        Log.d(TAG,"publicKey2"+Base64.encodeToString(publicKey.getEncoded(), Base64.DEFAULT));
+        cipher.init(Cipher.ENCRYPT_MODE,publicKey);
+
+        byte[] publicKeyBytes = Base64.encode(publicKey.getEncoded(),0);
+        String pubKey = new String(publicKeyBytes);
+        Log.d(TAG,"publicKey4444"+ pubkey3);
+
+        byte[] encryptedBytes = cipher.doFinal(text.getBytes("utf-8"));
+        Log.d(TAG, "encry"+encryptedBytes.toString());
+        return encryptedBytes;
+    }
+
+    public String decrypt(final byte[] encryptedText){
+        try{
+            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+            PrivateKey privateKey = (PrivateKey) keyStore.getKey("key1", null);
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE,privateKey);
+            byte[] decryptedText = cipher.doFinal(encryptedText);
+            Log.d(TAG, "end decrypt" + decryptedText.toString());
+            return new String(decryptedText);
+        }catch (UnrecoverableKeyException | IOException | KeyStoreException | CertificateException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e){
+            Log.e("decrypt catch", e.getMessage()+"");
+            String text = new String(encryptedText);
+            return text;
+        }
+
     }
 
 }
