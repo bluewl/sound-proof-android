ecb363e9e8c6dad711ebe0ca0e3a7fc3f9c34561
Wooseok Lee <111leews@gmail.com>
2022-03-24 14:47:02 -0400

    one third octave filtering complete

---
+++ app/src/main/java/com/example/sound_proof_android/OneThirdOctaveBands.java
@@ -0,0 +1,249 @@
+package com.example.sound_proof_android;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by G. Guillaume on 02/06/2015.
+ * Third octave bands filtering of a time signal
+ * This module applies an third octave bands filters according to the standard IEC-61260 "Electroacoustics - Octave-band and fractional-octave-band filters" (2001)
+ * @see <a href="http://siggigue.github.io/pyfilterbank/splweighting.html">http://siggigue.github.io/pyfilterbank/splweighting.html</a>
+ * @see <a href="http://www.mathworks.com/matlabcentral/fileexchange/69-octave/content//octave/oct3dsgn.m">http://www.mathworks.com/matlabcentral/fileexchange/69-octave/content//octave/oct3dsgn.m</a>
+ */
+public class OneThirdOctaveBands {
+
+    public enum FREQUENCY_BANDS {REDUCED, FULL};
+    private int expectedSampleLength;
+    private int samplingRate;
+    private final double[] standardFrequencies;
+    private Context current;
+
+    private List<FiltersParameters> filterParameters;
+
+    /**
+     * Standard center frequencies of third octave bands
+     * STANDARD_FREQUENCIES_REDUCED corresponds with a reduced array of standard third octave bands frequencies in the range [100Hz, 20kHz]
+     * STANDARD_FREQUENCIES_FULL corresponds with the array of standard third octave bands frequencies in the range [100Hz, 20kHz]
+     */
+    public static final double[] STANDARD_FREQUENCIES_REDUCED = new double[]{100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000};
+    public static final double[] STANDARD_FREQUENCIES_FULL = new double[]{16, 20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000};
+
+    /**
+     * Third octave bands filtering constructor
+     */
+    public OneThirdOctaveBands(int samplingRate, FREQUENCY_BANDS frequency_bands, Context current) {
+        this.samplingRate = samplingRate;
+        this.standardFrequencies = getStandardFrequencies(frequency_bands);
+        String csvFileName;
+        if (frequency_bands == FREQUENCY_BANDS.FULL) {
+            // Third octave bands filtering over the full standards frequency bands (i.e. [16Hz-20kHz]) requires a
+            // 5-seconds duration input signal
+            this.expectedSampleLength = samplingRate * 5;
+            csvFileName = "Third_oct_filters_coefts_44100Hz_16Hz-20kHz";
+        } else {
+            // Third octave bands filtering over the full standards frequency bands (i.e. [100Hz-20kHz]) requires a
+            // 1-second duration input signal
+            this.expectedSampleLength = samplingRate;
+            csvFileName = "Third_oct_filters_coefts_44100Hz_100Hz-20kHz.csv";
+        }
+        filterParameters = new ArrayList<FiltersParameters>(standardFrequencies.length);
+        this.current = current;
+        InputStream input = this.current.getResources().openRawResource(R.raw.filter_coef);
+        getFiltersParameters(input);
+    }
+
+    public static double[] getStandardFrequencies(FREQUENCY_BANDS frequency_bands) {
+        if (frequency_bands == FREQUENCY_BANDS.FULL) {
+            return STANDARD_FREQUENCIES_FULL;
+        } else {
+            return STANDARD_FREQUENCIES_REDUCED;
+        }
+    }
+
+    public static double getSampleBufferDuration(FREQUENCY_BANDS frequency_bands) {
+        if (frequency_bands == FREQUENCY_BANDS.FULL) {
+            return 5.;
+        } else {
+            return 1.;
+        }
+    }
+
+    /**
+     * Get the array of standard third octave bands frequencies
+     * @param samplingRate sampling rate [Hz]
+     * @param sampleDuration sample duration [s]
+     * @return array of standard third octave bands frequencies
+     */
+    public final double[] getStandardFrequencies(int samplingRate, double sampleDuration) {
+        double[] standFrequencies = new double[0];
+        if (samplingRate == 44100 && sampleDuration == 1.) {
+            standFrequencies = STANDARD_FREQUENCIES_REDUCED;
+        }
+        else if (samplingRate == 44100 && sampleDuration == 5.) {
+            standFrequencies = STANDARD_FREQUENCIES_FULL;
+        }
+        return standFrequencies;
+    }
+
+
+    /**
+     * Load the .csv file containing the third octave bands filters parameters
+     * @param csvFile input stream of the parameters file
+     */
+    private void getFiltersParameters(InputStream csvFile) {
+        BufferedReader inputStream = new BufferedReader(new InputStreamReader(csvFile));
+        try {
+            String line;
+            FiltersParameters lastParam = null;
+            while ((line = inputStream.readLine()) != null) {
+                StringTokenizer splitter = new StringTokenizer(line, ",");
+                double frequency = Double.valueOf(splitter.nextToken());
+                splitter.nextToken();
+                double[] params = new double[5];
+                int i = 0;
+                while(splitter.hasMoreTokens()) {
+                    params[i++] = Double.valueOf(splitter.nextToken());
+                }
+                if(lastParam != null && lastParam.frequency != frequency) {
+                    filterParameters.add(lastParam);
+                    lastParam = new FiltersParameters(frequency);
+                }
+                if(lastParam == null) {
+                    lastParam = new FiltersParameters(frequency);
+                }
+                StageParameters stageParameters = new StageParameters(params);
+                lastParam.stages.add(stageParameters);
+            }
+            filterParameters.add(lastParam);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                // Ignore error
+            }
+        }
+    }
+
+    /**
+     * @return List of parameters used to filter the signal.
+     */
+    public List<FiltersParameters> getFilterParameters() { return filterParameters; }
+
+    /**
+     * Second-order recursive linear filtering
+     * @param signal Raw time input signal
+     * @param filterParams Third octave band filter coefficients
+     * @param states State variables array
+     */
+    private void sosFiltering(final double[] signal, FiltersParameters filterParams, double[][] states){
+
+        // Loop on the cascaded filtering stages
+        int k = 0;
+        for (StageParameters stage: filterParams.stages){
+            double w1 = states[0][k];
+            double w2 = states[1][k];
+
+            // Feedforward coefficients
+            final double b0 = stage.coefficients[0];
+            final double b1 = stage.coefficients[1];
+            final double b2 = stage.coefficients[2];
+
+            // Feedback coefficients
+            final double a1 = stage.coefficients[3];
+            final double a2 = stage.coefficients[4];
+
+            // Second-order recursive linear filtering
+            for (int idT = 0; idT < signal.length; ++idT){
+                final double w0 = signal[idT] - a1*w1 - a2*w2;
+                signal[idT] = b0*w0 + b1*w1 + b2*w2;
+                w2 = w1;
+                w1 = w0;
+            }
+            states[0][k] = w1;
+            states[1][k] = w2;
+            k++;
+        }
+    }
+
+    /**
+     * Reverse a double 2D array
+     * @param arr double 2D array
+     * @return the reverses double 2D array
+     */
+    private double[] reverse2dArray(double[] arr){
+        double[] reversedArray = new double[arr.length];
+        for(int id = 0; id < arr.length; id++) {
+            reversedArray[arr.length - id - 1] = arr[id];
+        }
+        return reversedArray;
+    }
+
+    /**
+     * Signal backward and forward filtering by second-order recursive linear
+     * @param signal Raw time input signal
+     * @param idFreq Index of the central frequency
+     * @return Third octave band filtered signal
+     */
+    private double[] applySosFilter(double[] signal, int idFreq){
+        // Check the audio input sample duration
+        //if(signal.length != expectedSampleLength) {
+        //    throw new IllegalArgumentException("Illegal audio sample duration: expected " + expectedSampleLength + ", got " + signal.length);
+        //}
+        double [][] states = new double [2][4];
+        FiltersParameters filtParams = this.filterParameters.get(idFreq);
+
+        // Backward filtering
+        double[] reversedSignal = reverse2dArray(signal);
+        sosFiltering(reversedSignal, filtParams, states);
+
+        // Forward filtering
+        double[] reversedBackFilteredSignal = reverse2dArray(reversedSignal);
+        sosFiltering(reversedBackFilteredSignal, filtParams, states);
+
+        return reversedBackFilteredSignal;
+    }
+
+    /**
+     * Third octave filtering
+     * @param signal Raw time input signal
+     */
+    public double[][] thirdOctaveFiltering(double[] signal){
+        int signalLength = signal.length;
+        int nbFreqs = standardFrequencies.length;
+        double [][] filteredSignals = new double[nbFreqs][signalLength];
+        for (int idf = 0; idf < nbFreqs; idf++){
+            filteredSignals[idf] = applySosFilter(signal, idf);
+        }
+        return filteredSignals;
+    }
+
+    /**
+     * Cascade stage parameters
+     */
+    public static class StageParameters {
+
+        // Filter coefficients for one frequency and one stage
+        public final double[] coefficients;
+
+        public StageParameters(double[] coefficients) {
+            this.coefficients = coefficients;
+        }
+    }
+
+    /**
+     * Third octave filters parameters
+     */
+    public static class FiltersParameters {
+        public final double frequency;
+        public final List<StageParameters> stages = new ArrayList<StageParameters>();
+
+        public FiltersParameters(double frequency) {
+            this.frequency = frequency;
+        }
+    }
+}
--- app/src/main/java/com/example/sound_proof_android/ui/process/ProcessFragment.java
+++ app/src/main/java/com/example/sound_proof_android/ui/process/ProcessFragment.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.sound_proof_android.OneThirdOctaveBands;
 import com.example.sound_proof_android.R;
 
 import java.io.File;
@@ -20,6 +21,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static java.nio.ByteOrder.BIG_ENDIAN;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
@@ -44,67 +46,15 @@ public class ProcessFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 try {
-                    ContextWrapper contextWrapper = new ContextWrapper(getActivity().getApplicationContext());
-                    File audioDirectory = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
-                    String audioFileDirectory = audioDirectory + "/soundproof.wav";
-                    System.out.println("directory " + audioFileDirectory);
-                    File file = new File(audioFileDirectory);
-                    InputStream fileInputstream = new FileInputStream(file);
-                    ByteBuffer byteBuffer;
-                    for(int i=0; i<numberOfBytes.length; i++){
-                        byte byteArray[] = new byte[numberOfBytes[i]];
-                        int r = fileInputstream.read(byteArray, 0, numberOfBytes[i]);
-                        byteBuffer = ByteArrayToNumber(byteArray, numberOfBytes[i], type[i]);
-                        if (i == 0) {chunkID =  new String(byteArray); System.out.println("chunkID " + chunkID);}
-                        if (i == 1) {chunkSize = byteBuffer.getInt(); System.out.println("chunkSize " + chunkSize);}
-                        if (i == 2) {format =  new String(byteArray); System.out.println("format " + format);}
-                        if (i == 3) {subChunk1ID = new String(byteArray);System.out.println("subChunk1ID " + subChunk1ID);}
-                        if (i == 4) {subChunk1Size = byteBuffer.getInt(); System.out.println("subChunk1Size " + subChunk1Size);}
-                        if (i == 5) {audioFomart = byteBuffer.getShort(); System.out.println("audioFomart " + audioFomart);}
-                        if (i == 6) {numChannels = byteBuffer.getShort();System.out.println("numChannels " + numChannels);}
-                        if (i == 7) {sampleRate = byteBuffer.getInt();System.out.println("sampleRate " + sampleRate);}
-                        if (i == 8) {byteRate = byteBuffer.getInt();System.out.println("byteRate " + byteRate);}
-                        if (i == 9) {blockAlign = byteBuffer.getShort();System.out.println("blockAlign " + blockAlign);}
-                        if (i == 10) {bitsPerSample = byteBuffer.getShort();System.out.println("bitsPerSample " + bitsPerSample);}
-                        if (i == 11) {
-                            subChunk2ID = new String(byteArray) ;
-                            if(subChunk2ID.compareTo("data") == 0) {
-                                continue;
-                            }
-                            else if( subChunk2ID.compareTo("LIST") == 0) {
-                                byte byteArray2[] = new byte[4];
-                                r = fileInputstream.read(byteArray2, 0, 4);
-                                byteBuffer = ByteArrayToNumber(byteArray2, 4, 1);
-                                int temp = byteBuffer.getInt();
-                                //redundant data reading
-                                byte byteArray3[] = new byte[temp];
-                                r = fileInputstream.read(byteArray3, 0, temp);
-                                r = fileInputstream.read(byteArray2, 0, 4);
-                                subChunk2ID = new String(byteArray2) ;
-                            }
-                        }
-                        if (i == 12) {subChunk2Size = byteBuffer.getInt();System.out.println("subChunk2Size " + subChunk2Size);}
-                    }
-                    bytePerSample = bitsPerSample/8;
-                    float value;
-                    ArrayList<Float> dataVector = new ArrayList<>();
-                    while (true){
-                        byte byteArray[] = new byte[bytePerSample];
-                        int v = fileInputstream.read(byteArray, 0, bytePerSample);
-                        value = convertToFloat(byteArray,1);
-                        dataVector.add(value);
-                        if (v == -1) break;
-                    }
-                    float data [] = new float[dataVector.size()];
-                    for(int i=0;i<dataVector.size();i++){
-                        data[i] = dataVector.get(i);
-                    }
-                    // System.out.println("Total data bytes "+sum);
-                    // return data;
-                    System.out.println("**********************************************************");
-                    System.out.print(data.length);
-                    System.out.println();
-                    System.out.println("**********************************************************");
+                    double[] data = readWav();
+                    System.out.println("Before creating One Third Octave Class ******");
+                    OneThirdOctaveBands third = new OneThirdOctaveBands(sampleRate, OneThirdOctaveBands.FREQUENCY_BANDS.REDUCED, getActivity());
+                    System.out.println("After creating One Third Octave Class ******");
+                    double[][] filteredSignal = third.thirdOctaveFiltering(data);
+                    System.out.println("*******************************************************");
+                    System.out.println(filteredSignal.length);
+                    System.out.println(filteredSignal[0].length);
+                    System.out.println("*******************************************************");
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -137,4 +87,64 @@ public class ProcessFragment extends Fragment {
         return (float) buffer.getShort();
     }
 
+    public double[] readWav() throws IOException {
+        ContextWrapper contextWrapper = new ContextWrapper(getActivity().getApplicationContext());
+        File audioDirectory = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        String audioFileDirectory = audioDirectory + "/soundproof.wav";
+        System.out.println("directory " + audioFileDirectory);
+        File file = new File(audioFileDirectory);
+        InputStream fileInputstream = new FileInputStream(file);
+        ByteBuffer byteBuffer;
+        for(int i=0; i<numberOfBytes.length; i++){
+            byte byteArray[] = new byte[numberOfBytes[i]];
+            int r = fileInputstream.read(byteArray, 0, numberOfBytes[i]);
+            byteBuffer = ByteArrayToNumber(byteArray, numberOfBytes[i], type[i]);
+            if (i == 0) {chunkID =  new String(byteArray); System.out.println("chunkID " + chunkID);}
+            if (i == 1) {chunkSize = byteBuffer.getInt(); System.out.println("chunkSize " + chunkSize);}
+            if (i == 2) {format =  new String(byteArray); System.out.println("format " + format);}
+            if (i == 3) {subChunk1ID = new String(byteArray);System.out.println("subChunk1ID " + subChunk1ID);}
+            if (i == 4) {subChunk1Size = byteBuffer.getInt(); System.out.println("subChunk1Size " + subChunk1Size);}
+            if (i == 5) {audioFomart = byteBuffer.getShort(); System.out.println("audioFomart " + audioFomart);}
+            if (i == 6) {numChannels = byteBuffer.getShort();System.out.println("numChannels " + numChannels);}
+            if (i == 7) {sampleRate = byteBuffer.getInt();System.out.println("sampleRate " + sampleRate);}
+            if (i == 8) {byteRate = byteBuffer.getInt();System.out.println("byteRate " + byteRate);}
+            if (i == 9) {blockAlign = byteBuffer.getShort();System.out.println("blockAlign " + blockAlign);}
+            if (i == 10) {bitsPerSample = byteBuffer.getShort();System.out.println("bitsPerSample " + bitsPerSample);}
+            if (i == 11) {
+                subChunk2ID = new String(byteArray) ;
+                if(subChunk2ID.compareTo("data") == 0) {
+                    continue;
+                }
+                else if( subChunk2ID.compareTo("LIST") == 0) {
+                    byte byteArray2[] = new byte[4];
+                    r = fileInputstream.read(byteArray2, 0, 4);
+                    byteBuffer = ByteArrayToNumber(byteArray2, 4, 1);
+                    int temp = byteBuffer.getInt();
+                    //redundant data reading
+                    byte byteArray3[] = new byte[temp];
+                    r = fileInputstream.read(byteArray3, 0, temp);
+                    r = fileInputstream.read(byteArray2, 0, 4);
+                    subChunk2ID = new String(byteArray2) ;
+                }
+            }
+            if (i == 12) {subChunk2Size = byteBuffer.getInt();System.out.println("subChunk2Size " + subChunk2Size);}
+        }
+        bytePerSample = bitsPerSample/8;
+        float value;
+        ArrayList<Float> dataVector = new ArrayList<>();
+        while (true){
+            byte byteArray[] = new byte[bytePerSample];
+            int v = fileInputstream.read(byteArray, 0, bytePerSample);
+            value = convertToFloat(byteArray,1);
+            dataVector.add(value);
+            if (v == -1) break;
+        }
+        double[] data = new double[dataVector.size()];
+        for(int i=0;i<dataVector.size();i++){
+            data[i] = dataVector.get(i);
+        }
+        System.out.println("******** signal length ********");
+        System.out.println(data.length);
+        return data;
+    }
 }
